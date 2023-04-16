95ca59030023ed3e34ff9d328ff6bd95f63e0a8c
Дмитрий <93095763+dmitriy152@users.noreply.github.com>
2023-04-16 20:18:26 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,43 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+/*require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';*/
+$mail = new PHPMailer();
+$mail->CharSet = 'utf-8';
+$mail->SMTPDebug  = 1;
+
+
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+                              // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'dmitriyphpmailer@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'fgu2CSrF6tsxAd73kSRk'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('dmitriyphpmailer@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('giwafo2263@ippals.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = ' оставили заявку, Телефон этого пользователя ' .$phone. '<br>Почта этого пользователя: ' .$email;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: index.html');
+}
+?>
